70290b68353ae4140d6053e18f8cae2e3a027341
Marcos1327 <giovannygigi@gmail.com>
2023-06-18 18:59:04 -0300

    Added relationship user and post and added Swagger config working

--- src/main/java/com/personalblog/entities/Post.java
+++ src/main/java/com/personalblog/entities/Post.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,6 +28,10 @@ public class Post implements Serializable {
 	private String content;
 	
 	private String category;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "id_user", nullable = false)
+	private User user;
 	
 	@Column(nullable = false)
 	@JsonFormat(pattern = "yyyy-MM-dd")
@@ -84,6 +90,14 @@ public class Post implements Serializable {
 
 	public void setModifiedDate(LocalDateTime modifiedDate) {
 		this.modifiedDate = modifiedDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 	
 	
